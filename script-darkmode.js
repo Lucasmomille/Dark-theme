3c1b5e077990a19c6e516c02f5cc821c43e339a7
Lucas Moquet <lucas.moquet95@gmail.com>
2021-01-18 15:37:45 +0100

    dark mode switch css

--- script-darkmode.js
+++ script-darkmode.js
@@ -1,17 +1,25 @@
+/*jslint browser */
+/*global window*/
 
+window.onload = function(){
+    
 /* Definir variables utilisée (le bouton et le thème) */
-let btn = document.getElementByID("#customSwitch1");
+var btn = document.querySelector("#customSwitch1");
+console.log(btn);
 
-let theme = document.getElementByID("#theme-link");
+var theme = document.querySelector("#theme-link");
 
 /* Au clic du bouton effectuer une action */
 
 btn.addEventListener("click", function() {
     /* si theme css normal alors utiliser le lien dark */
-    if (document.getAttribute("href") == style.css ) {
-        theme.href = "style-dark.css";
+    if (theme.getAttribute("href") == "css/style.css" ) {
+        theme.href = "css/style-darkmode.css";
     /* si theme css dark alors utiliser le lien normal */
     } else {
-        theme.href = "style.css";
+        theme.href = "css/style.css";
     }
 })
+
+
+}
